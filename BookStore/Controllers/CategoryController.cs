7d8e1bb48c0cff351e4b1702bfa35a5d4ee9d5f9
Sefa <sefakesici1220@gmail.com>
2022-12-17 00:33:10 +0300

    First Commit

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            var CategoryList = _db.Kategori.ToList();
+            return View(CategoryList);
+        }
+    }
+}
---
+++ BookStore/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Kategori { get; set; } 
+    }
+}
